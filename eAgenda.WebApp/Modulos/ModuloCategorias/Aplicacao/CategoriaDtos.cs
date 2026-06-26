e47e492d3247d1d819ee785d597846310da4082d
Marco-Oliver <marcooliver2@yahoo.com>
2026-06-26 14:20:26 -0300

    refactor(dto): adjust data transfer objects structure and mappings

--- eAgenda.WebApp/Modulos/ModuloCategorias/Aplicacao/CategoriaDtos.cs
+++ eAgenda.WebApp/Modulos/ModuloCategorias/Aplicacao/CategoriaDtos.cs
@@ -1,9 +1,21 @@
+
 namespace eAgenda.WebApp.Modulos.ModuloCategorias.Aplicacao;
 
-public record ListarCategoriasDto(Guid Id, string Titulo);
+public record ListarCategoriasDto(
+    Guid Id,
+    string Titulo
+    );
 
-public record CadastrarCategoriaDto(string Titulo);
+public record CadastrarCategoriaDto(
+    string Titulo
+    );
 
-public record EditarCategoriaDto(Guid Id, string Titulo);
+public record EditarCategoriaDto(
+    Guid Id, 
+    string Titulo
+    );
 
-public record DetalhesCategoriaDto(Guid Id, string Titulo);
+public record DetalhesCategoriaDto(
+    Guid Id, 
+    string Titulo
+);
